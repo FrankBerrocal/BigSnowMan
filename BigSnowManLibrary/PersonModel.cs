8c4f3ab9f829382c1524c7de80579182a285283b
Frank <66488543+FrankBerrocal@users.noreply.github.com>
2022-11-03 03:05:27 -0600

    C# base in VS Code
    1.  Complete Solution structure created.
    2.  Library, solution and Console created.
    3.   Packages Added.
    4.  SQL connection added, need to make it work all together.
    5.  No errors reported.

---
+++ BigSnowManLibrary/PersonModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace BigSnowManLibrary
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }    
+
+        public int Age { get; set; }    
+    }
+}
