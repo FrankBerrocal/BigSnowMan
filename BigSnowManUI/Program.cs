c109bb17e7a261440d59767035666ad281ed7787
Frank <66488543+FrankBerrocal@users.noreply.github.com>
2022-11-03 03:38:39 -0600

    2.  Inclusion of SQL code and connector
    I have configured several queries using SQL driver in VSCode.
    I am trying to replicate the Professor's code but i believe my error lies on the specifications of the connection.

--- BigSnowManUI/Program.cs
+++ BigSnowManUI/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using BigSnowManLibrary;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace BigSnowManUI
 {
@@ -15,10 +17,46 @@ namespace BigSnowManUI
             };
 
             System.Console.WriteLine("hello world!");
-            System.Console.WriteLine("This is a test");
-            System.Console.WriteLine("The End");
+
 
             System.Console.WriteLine($"{person.FirstName} {person.LastName} is my name, and my age is {person.Age}");
+
+
+            //testing connection
+            string connectionString =
+                "Data Source =  (localhost/1433); Initial Catalog = AP" + "Integrated Security = true";
+
+            string queryString = 
+                "SELECT animalID, animalName, animalEats FROM AP.dbo.animal";
+
+            using(System.Data.SqlClient.SqlConnection connection = 
+                    new System.Data.SqlClient.SqlConnection(connectionString))
+                    {
+                        System.Data.SqlClient.SqlCommand command = 
+                            new System.Data.SqlClient.SqlCommand(queryString, connection);
+                        string test = connection.ToString();
+                        try
+                        {
+                            connection.Open();
+                            System.Data.SqlClient.SqlDataReader reader = command.ExecuteReader();
+                            while(reader.Read())
+                            {
+                                System.Console.WriteLine("\t{0}\t{1}\t{2}",
+                                    reader[0], reader[1], reader[2]);
+                            }    
+                            reader.Close();    
+                        }
+                        catch (Exception ex)
+                        {
+                            System.Console.WriteLine("Connection failed " + ex.Message);
+                        }
+                        Console.ReadLine();
+                    }
+
+
+           
+
+
         }
     }
 }
