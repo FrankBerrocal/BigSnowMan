23c82ea1468a3e05b3593ad09191076870207f1b
Frank <66488543+FrankBerrocal@users.noreply.github.com>
2022-11-10 17:06:41 -0600

    Creation of object Builder
    abstraction of SQLConnectionBuilder .

---
+++ VS_C#/SQL_DML_TestAPP/SQL_DML_Test/ObjectTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace SQL_DML_Test 
+{
+	public class ObjectTest
+	{
+        string dataSource = "localhost, 1440";
+        string userId = "sa";
+        string password = "myPassw0rd";
+        string initialCatalog = "master";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                BuilderObject builder = new BuilderObject(dataSource, userId, password, initialCatalog);
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+}
+
