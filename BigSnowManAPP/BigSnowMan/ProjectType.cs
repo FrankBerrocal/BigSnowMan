c3a155e9bfa9ccdcf59e6655441b6859962226ed
Frank <66488543+FrankBerrocal@users.noreply.github.com>
2022-12-07 22:48:38 -0600

    Creation of Objects
    Finally on c# codification.
    Creation of DB object, and selection objects (Status, ProjectType, etc) using generic objects added to a dictionary, that act as a scale for the description and the database ID.

---
+++ BigSnowManAPP/BigSnowMan/ProjectType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+public class ProjectTypeObject
+{
+    public string Type;
+    private int databaseID;
+
+    public ProjectTypeObject(String _type)
+    {
+        Type = _type;
+    }
+    public string getStatus()
+    {
+        return Type;
+    }
+    public override string ToString()
+    {
+        return Type;
+    }
+}
+
+
+
+
