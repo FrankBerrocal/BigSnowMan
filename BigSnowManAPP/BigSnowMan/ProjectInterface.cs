eace87a346b013fe1bdaf977007643ce57e5a3fa
Frank <66488543+FrankBerrocal@users.noreply.github.com>
2022-12-11 04:15:54 -0600

    Checking project status.
    All OK.  Backups in place.  Conflict resolved.

--- BigSnowManAPP/BigSnowMan/ProjectInterface.cs
+++ BigSnowManAPP/BigSnowMan/ProjectInterface.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/*****************************************************************
+ * Project Management Office Evaluation System (Prototype)       
+ * PMOES                                                                               
+ *                                                                                           
+ * Final Project                                                                                                                                       
+ *                                                                                            
+ * Frank Berrocal - 427887                                                      
+ * SODV2202 - Object Oriented Programming                            
+ * Prof.  Dr. Sohaib Bajwa                                                         
+ *                                
+ * 
+ * Bow Valley College
+ * December 2022
+ ******************************************************************/
+
+
+using System;
+
 namespace BigSnowMan
 {
 	public interface ProjectInterface
